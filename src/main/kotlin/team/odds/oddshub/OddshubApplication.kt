24566edb0482cea2132ec48e40326c108e6dc066
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-06 16:35:44 +0700

    initial project

---
+++ src/main/kotlin/team/odds/oddshub/OddshubApplication.kt
@@ -0,0 +1,11 @@
+package team.odds.oddshub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OddshubApplication
+
+fun main(args: Array<String>) {
+	runApplication<OddshubApplication>(*args)
+}
---
+++ src/test/kotlin/team/odds/oddshub/OddshubApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.odds.oddshub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OddshubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
