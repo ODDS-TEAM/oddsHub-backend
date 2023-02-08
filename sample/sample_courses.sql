8e8ed320756959c8d7a53f654da44f582b0d7697
Pakornpat Sinjiranon <skyfox.ku@gmail.com>
2023-02-08 16:21:05 +0700

    Add sample data sql for courses

---
+++ sample/sample_courses.sql
@@ -0,0 +1,67 @@
+INSERT INTO public.course (id,"name",description,image,instructor) VALUES
+	 (1,'CCO','CCO Hello world',NULL,NULL),
+	 (2,'DESIGN THINKING','Fundamental: Why What and How of Design Thinking
+Workshop: How to facilitates and tools recommend
+Incremental: Do design thininkg by team',NULL,'คุณเต๋า'),
+	 (3,'SERVICE DESIGN','Day 1: 
+Introduction
+Research 101
+Synthesis
+Ideate
+Idea Validation
+Touchpoint
+Stakeholder map
+Blueprint
+
+Day 2: 
+Service prototype
+Prototype
+Test',NULL,'คุณเต๋า'),
+	 (4,'AGILE FOR EXECUTIVES','Flexibility and adaptability has became vital attribute to survive in many business industries. This course is designed to share how Agile practices helps increase agility in business and what we should consider before start an adoption.
+
+Our Executive Briefing on Agile Development is an interactive presentation that provides a brief overview of Agile, along with best practices, tools, and challenges associated with the adoption of an Agile. It addresses organizational impact, distributed teams, agile leadership, and techniques and metrics for agile transformations, at a level appropriate for executives and managers.
+
+The purpose of the presentation is to provide a sense for what the audience members and their organizations will experience, and need to do, when they transition to an agile organization. Clients should allow approximately three hours for this presentation.
+
+Our Executive Briefing on Agile Development sessions provide tremendous value for very little investment in time. They provide an excellent introduction to the practices and issues that organizations, managers, and executives face when introducing an agile process.',NULL,'รูฟ เจน จั๊วะ'),
+	 (5,'BEING FACILITATOR','Facilitation skills are the abilities to provide opportunities and environment to a group of people that enable them to make progress and succeed. Facilitation skills are an essential component of effective meetings, brainstroming, decision making or even daily work.
+
+By having a facilitation skilled, these can supercharge a team’s performance by functioning as a process guide for navigating complicated business challenges throught various facilitation techniques. Facilitation skill also essential for creating collarorative working envirement in a group and organization to create a safe and colloaborative working environment that everyone can contribute and driving the success together.
+
+In this workshop participant will learn below topics based on a group activities.
+- What is the role of facilitator?
+- What does Facailitator do?
+- Facilitation tools?
+- Put it into practice.
+
+This class is a collection of facilitation techniques we continually use in workshops, training courses, meetings and retrospectives. By the end of session paricipant will clearly understand the role of facilitator and apply tool technique from the session to their work.',NULL,'คุณแอร์ คุณจิ๊บ'),
+	 (6,'DATA THINKING','Data Thinking course outline
+
+Morning (Concept)
+
+* Data + Design Thinking: เรียนรู้ว่าข้อมูลเข้ามาช่วนในกระบวนการออกแบบอย่างไร
+* Job to be done: เริ่มต้นจากปัญหาของผู้ใช้ มองหาตัวชี้วัด Progress 
+* HMW for data: กำหนด HMW พร้อมข้อมูลที่ใช้วิเคราะห์ความเป็นไปได้ของ HMW นั้นๆ
+
+Afternoon (Tools)
+
+* PowerBI: เครื่องมือ Tableau ในการวิเคราะห์ข้อมูล
+* Data Visualization: วิธีอธิบายข้อมูลให้ผู้อ่านสามารถเข้าใจได้อย่างรวดเร็ว
+* Idea supported by Data: คิด idea การแก้ปัญหาแล้วทดลองหาข้อมูลรองรับแนวคิดนั้น
+* Pitch: อธิบาย idea ให้เพื่อนฟังพร้อมข้อมูลที่ทำให้เห็นความเป็นไปได้',NULL,'คุณแบงค์'),
+	 (7,'COLLECTIVE PRODUCT OWNERSHIP',NULL,NULL,'คุณแอร์ คุณจิ๊บ'),
+	 (8,'FLIGHT LEVELS','Day 1
+- The Theory of Constraint
+- Workflow Visualization
+- WIP in Action
+
+Day 2
+- Biz Agility vs. Agile Team 
+- Flight Levels Architecture
+- Flight Item & Flight Route
+- Flight Levels with Organization Strategy 
+- Agile Interaction',NULL,'คุณโอฬาร'),
+	 (9,'PROCESS VISUALIZATION & DESIGN (RAPID PROTOTYPING)',NULL,NULL,'คุณแบงค์'),
+	 (10,'VISUAL NOTES',NULL,NULL,'คุณจิ๊บ');
+INSERT INTO public.course (id,"name",description,image,instructor) VALUES
+	 (11,'AGILE PROJECT MANAGEMENT','With Agile principles and techniques, Agile project management is the application of processes, methods, skills, knowledge and experience to achieve specific project objectives according to the project acceptance criteria within agreed parameters.',NULL,'คุณรูฟ คุณจิ๊บ คุณแอร์');
