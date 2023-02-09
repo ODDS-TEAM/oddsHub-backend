9dc854c35bade7b24874b1068fcb710ced50c0e1
Jirat Chokdeeanan <jirat.cho@gmail.com>
2023-02-09 15:38:47 +0700

    import liquibase

---
+++ src/main/resources/db/changelog/ODDS-HUB/DDL/001_create_tables.sql
@@ -0,0 +1,28 @@
+--changeset liquibase:create-table-odds-hub
+CREATE TABLE classes
+(
+    id          SERIAL PRIMARY KEY,
+    course_id   SERIAL,
+    start_date timestamp,
+    end_date timestamp
+);
+
+CREATE TABLE courses
+(
+    id          SERIAL PRIMARY KEY,
+    description text,
+    image       text,
+    instructor  varchar(255),
+    name        varchar(255)
+);
+
+CREATE TABLE registration_users
+(
+    id         SERIAL PRIMARY KEY,
+    class_id   SERIAL,
+    email varchar(255),
+    first_name varchar(255),
+    last_name varchar(255),
+    phone varchar(255),
+    title varchar(255)
+);
