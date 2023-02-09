9a8c6b9cf3c99be1411080b4ce7bee07a5a7ce00
TopToppy <toptoppy@odds.team>
2023-02-09 14:14:20 +0700

    modified table name

--- src/main/kotlin/team/odds/oddshub/entities/ClassEntity.kt
+++ src/main/kotlin/team/odds/oddshub/entities/ClassEntity.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.*
 import java.util.*
 
 @Entity
+@Table(name = "classes")
 data class ClassEntity(
     @Id
     @GeneratedValue
--- src/main/kotlin/team/odds/oddshub/entities/RegistrationUserEntity.kt
+++ src/main/kotlin/team/odds/oddshub/entities/RegistrationUserEntity.kt
@@ -3,9 +3,11 @@ package team.odds.oddshub.entities
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.*
 
 @Entity
+@Table(name = "registration_users")
 data class RegistrationUserEntity(
     @Id
     val id: UUID,
