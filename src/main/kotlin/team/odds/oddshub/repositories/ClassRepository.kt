c78b1d5db9e85f5bdc7aa178aecb5274a4bfecee
Jirat Chokdeeanan <jirat.cho@gmail.com>
2023-02-09 17:41:38 +0700

    Update courses payload

--- src/main/kotlin/team/odds/oddshub/repositories/ClassRepository.kt
+++ src/main/kotlin/team/odds/oddshub/repositories/ClassRepository.kt
@@ -3,7 +3,9 @@ package team.odds.oddshub.repositories
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import team.odds.oddshub.entities.ClassEntity
+import java.util.Optional
 
 @Repository
 interface ClassRepository: JpaRepository<ClassEntity, Long> {
+    fun findByCourseId(courseId: Long): Optional<ClassEntity>
 }
--- src/test/kotlin/team/odds/oddshub/controllers/CoursesControllerTest.kt
+++ src/test/kotlin/team/odds/oddshub/controllers/CoursesControllerTest.kt
@@ -3,14 +3,21 @@ package team.odds.oddshub.controllers
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import team.odds.oddshub.entities.ClassEntity
 import team.odds.oddshub.entities.CourseEntity
+import team.odds.oddshub.entities.dto.CoursePayload
+import team.odds.oddshub.repositories.ClassRepository
 import team.odds.oddshub.repositories.CourseRepository
 import team.odds.oddshub.services.CourseService
+import java.util.*
 
 class CoursesControllerTest {
 
     private var courseRepository: CourseRepository = mockk()
+    private var classRepository: ClassRepository = mockk()
     private val ccoCourse = CourseEntity(1, "CCO", "des", "image.png", "P'Roof", 30, 40000)
+    private val ccoClass = ClassEntity(1, 1, Date(), Date())
+    private val ccoPayload = CoursePayload(1, "CCO", "des", "P'Roof", 40000, Date())
 
     @Test
     fun `Not have any available course for user`() {
@@ -26,23 +33,25 @@ class CoursesControllerTest {
 
     private fun oddsHubNotHasAnyCourse() {
         every { courseRepository.findAll() } returns listOf()
+        every { classRepository.findByCourseId(any()) } returns Optional.empty()
     }
 
     private fun oddsHubHasOneCourse() {
         every { courseRepository.findAll() } returns listOf(ccoCourse)
+        every { classRepository.findByCourseId(any()) } returns Optional.of(ccoClass)
     }
 
-    private fun whenGetAllCourses(): List<CourseEntity> =
+    private fun whenGetAllCourses(): List<CoursePayload> =
         CoursesController(
-            CourseService(courseRepository)
+            CourseService(courseRepository, classRepository)
         ).getAllCourses()
 
-    private fun expectToHaveOneCourse(courses: List<CourseEntity>) {
+    private fun expectToHaveOneCourse(courses: List<CoursePayload>) {
         assert(courses.count() == 1)
-        assert(courses[0] == ccoCourse)
+        assert(courses[0] == ccoPayload)
     }
 
-    private fun expectToNotHaveAnyCourse(courses: List<CourseEntity>) {
+    private fun expectToNotHaveAnyCourse(courses: List<CoursePayload>) {
         assert(courses.isEmpty())
     }
 }
