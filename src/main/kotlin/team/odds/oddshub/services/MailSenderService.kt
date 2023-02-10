f07d5d36625ab3c32d8b9b3be8f7d958be64a604
Jirat Chokdeeanan <jirat.cho@gmail.com>
2023-02-10 12:40:23 +0700

    add some logs for additional info

--- src/main/kotlin/team/odds/oddshub/services/MailSenderService.kt
+++ src/main/kotlin/team/odds/oddshub/services/MailSenderService.kt
@@ -1,6 +1,7 @@
 package team.odds.oddshub.services
 
 import jakarta.mail.internet.MimeMessage
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
@@ -13,6 +14,8 @@ import team.odds.oddshub.entities.dto.WelcomeEmail
 class MailSenderService(
     val templateEngine: TemplateEngine
 ) {
+    private val logger = KotlinLogging.logger {}
+
     @Autowired
     private lateinit var javaMailSender: JavaMailSender
 
@@ -22,7 +25,9 @@ class MailSenderService(
 
     fun send(email: WelcomeEmail) {
         val message = createWelcomeMessage(email)
-        this.javaMailSender.send(message)
+        this.javaMailSender.send(message).also {
+            logger.info { "Email sent welcome message to ${email.to}" }
+        }
     }
 
     private fun createWelcomeMessage(email: WelcomeEmail): MimeMessage {
--- src/main/kotlin/team/odds/oddshub/services/RegistrationUserService.kt
+++ src/main/kotlin/team/odds/oddshub/services/RegistrationUserService.kt
@@ -1,5 +1,6 @@
 package team.odds.oddshub.services
 
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import team.odds.oddshub.entities.dto.RegistrationUserPayload
 import team.odds.oddshub.repositories.ClassRepository
@@ -12,14 +13,18 @@ class RegistrationUserService(
     val classRepository: ClassRepository,
     val courseRepository: CourseRepository,
 ) {
+    private val logger = KotlinLogging.logger {}
+
     fun saveRegistrationUser(registrationUserData: RegistrationUserPayload): Boolean {
         val classEntity = classRepository.getReferenceById(registrationUserData.classId)
         val courseEntity = courseRepository.getReferenceById(classEntity.courseId)
         val registrationUserEntityList = registrationUserRepository.getByClassId(registrationUserData.classId)
 
+        logger.info { "Course ${courseEntity.id} # of registered ${registrationUserEntityList.size}/${courseEntity.quota} " }
         if (courseEntity.quota > registrationUserEntityList.size) {
             val registrationUserDataEntity = registrationUserData.transformToEntity()
             registrationUserRepository.save(registrationUserDataEntity)
+                .also { logger.debug { "Registered Success ${it.id}" } }
             return true
         }
         return false
